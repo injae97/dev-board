b3ef756d45bedb23c104b8d96508b6e7222a56a0
InJeKim97 <injaekim97@naver.com>
2023-01-10 00:38:53 +0900

    feat/main/InJekim97 : 게시판 만들기 (CRUD)

--- src/main/java/bbs/BbsDAO.java
+++ src/main/java/bbs/BbsDAO.java
@@ -120,4 +120,27 @@ public class BbsDAO {
 		}
 		return false;
     }
+    
+    /* 게시글 제목 클릭시 내용 불러오기 위한 기능 */
+    public Bbs getBbs(int bbsID) {
+		String SQL = "SELECT * FROM BBS WHERE bbsID = ?";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			pstmt.setInt(1, bbsID);
+			rs = pstmt.executeQuery(); 
+			if (rs.next()) {
+				Bbs bbs = new Bbs();
+				bbs.setBbsID(rs.getInt(1)); 
+				bbs.setBbsTitle(rs.getString(2)); 
+				bbs.setUserID(rs.getString(3)); 
+				bbs.setBbsDate(rs.getString(4)); 
+				bbs.setBbsContent(rs.getString(5)); 
+				bbs.setBbsAvailable(rs.getInt(6)); 
+				return bbs;
+			}		
+		} catch (Exception e) {
+		    e.printStackTrace();	
+		}
+		return null; // 해당 게시글이 존재하지 않는다면 null
+    }
 }
